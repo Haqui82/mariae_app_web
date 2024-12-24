0ee9f522137fde661fe73a1ea6e5fe423d333c94
Haqui82 <mr.haqui82@gmail.com>
2024-12-23 21:29:27 -0500

    apuntandole a index.html

--- MariaE_AppWeb/MariaE_API/server.js
+++ MariaE_AppWeb/MariaE_API/server.js
@@ -21,12 +21,12 @@ app.use((req, res, next) => {
   next();
 });
 
-// Sirve archivos estáticos desde el directorio 'Front-End'
-app.use(express.static(path.join(__dirname, 'Front-End')));
+// Servir archivos estáticos desde el directorio "Front-End/MariaE-App_FrontEnd/html" 
+app.use(express.static(path.join(__dirname, '../../Front-End/MariaE-App_FrontEnd/html')));
 
-// Ruta para servir el archivo index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Front-End', 'html', 'index.html'));
+// Ruta para servir "index.html" para todas las demás solicitudes 
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '../../Front-End/MariaE-App_FrontEnd/html', 'index.html')); 
+  
 });
 
 // Importar y usar las rutas de post.js con el prefijo /api
