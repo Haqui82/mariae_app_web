4fbf26a0b5f604be6f450861fc6d5603a4a28ae1
Haqui82 <mr.haqui82@gmail.com>
2024-12-25 05:49:50 -0500

    completando atributos en db.js

--- MariaE_AppWeb/MariaE_API/config/code_backups/db_backup.js
+++ MariaE_AppWeb/MariaE_API/config/code_backups/db_backup.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config(); // Cargar variables de entorno desde .env
 
-// Configura la conexión a la base de datos
-const sequelize = new Sequelize('mariae_fashiongirls', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  db_port: process.env.DB_PORT
 });
 
 module.exports = sequelize;
--- MariaE_AppWeb/MariaE_API/config/db.js
+++ MariaE_AppWeb/MariaE_API/config/db.js
@@ -1,10 +1,19 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config(); // Cargar variables de entorno desde .env
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  db_port: process.env.DB_PORT
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT, // Asegúrate de que DB_PORT esté correcto
+    dialectOptions: {
+      connectTimeout: 10000 // Puedes aumentar el tiempo de espera si es necesario
+    }
+  }
+);
 
 module.exports = sequelize;
